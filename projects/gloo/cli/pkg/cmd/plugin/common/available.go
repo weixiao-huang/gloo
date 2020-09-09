dee1aae699cde35d02656ca5061271bb6821b107
Joe Kelley <jsphklly@gmail.com>
2020-09-09 14:56:08 -0400

    new list

---
+++ projects/gloo/cli/pkg/cmd/plugin/common/available.go
@@ -0,0 +1,181 @@
+package common
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/rotisserie/eris"
+	"github.com/solo-io/gloo/projects/gloo/cli/pkg/constants"
+	"github.com/spf13/cobra"
+)
+
+type InstalledPlugin struct {
+	Name     string
+	FullPath string
+	Warnings []error
+}
+
+type InstalledPluginRegistry interface {
+	ListAll() (plugins []InstalledPlugin, warnings []error, err error)
+	Uninstall(name string) error
+}
+
+type installedPluginRegistry struct {
+	verifier    PathVerifier
+	pluginPaths []string
+}
+
+func (r *installedPluginRegistry) Uninstall(name string) error {
+	// TODO joekelley
+	return nil
+}
+
+func NewInstalledPluginRegistry(cmd *cobra.Command) InstalledPluginRegistry {
+	r := &installedPluginRegistry{
+		verifier: &CommandOverrideVerifier{
+			root:        cmd.Root(),
+			seenPlugins: make(map[string]string),
+		},
+		pluginPaths: filepath.SplitList(os.Getenv("PATH")),
+	}
+
+	return r
+}
+
+func (r *installedPluginRegistry) ListAll() (plugins []InstalledPlugin, warnings []error, err error) {
+	for _, dir := range uniquePathsList(r.pluginPaths) {
+		if len(strings.TrimSpace(dir)) == 0 {
+			continue
+		}
+
+		files, err := ioutil.ReadDir(dir)
+		if err != nil {
+			warnings = append(warnings, eris.Errorf("unable to read directory %q in your PATH: %v", dir, err))
+			continue
+		}
+
+		for _, f := range files {
+			if f.IsDir() {
+				continue
+			}
+			if !hasValidPrefix(f.Name(), constants.ValidExtensionPrefixes) {
+				continue
+			}
+
+			plugin := InstalledPlugin{
+				Name:     f.Name(),
+				FullPath: filepath.Join(dir, f.Name()),
+			}
+
+			if errs := r.verifier.Verify(filepath.Join(dir, f.Name()), f.Name()); len(errs) != 0 {
+				for _, err := range errs {
+					plugin.Warnings = append(plugin.Warnings, err)
+				}
+			}
+
+			plugins = append(plugins, plugin)
+		}
+	}
+
+	return plugins, warnings, nil
+}
+
+// pathVerifier receives a path and determines if it is valid or not
+type PathVerifier interface {
+	// Verify determines if a given path is valid
+	Verify(path, binName string) []error
+}
+
+type CommandOverrideVerifier struct {
+	root        *cobra.Command
+	seenPlugins map[string]string
+}
+
+// Verify implements PathVerifier and determines if a given path
+// is valid depending on whether or not it overwrites an existing
+// glooctl command path, or a previously seen plugin.
+func (v *CommandOverrideVerifier) Verify(path, binName string) []error {
+	if v.root == nil {
+		return []error{eris.Errorf("unable to verify path with nil root")}
+	}
+
+	errors := []error{}
+
+	cmdPath := strings.Split(binName, "-")
+	if len(cmdPath) > 1 {
+		if cmdPath[0] != "glooctl" {
+			errors = append(errors, eris.Errorf("warning: the prefix should always be 'glooctl' for a plugin binary, found: %s", cmdPath[0]))
+			return errors
+		}
+		cmdPath = cmdPath[1:]
+	}
+
+	if isExec, err := isExecutable(path); err == nil && !isExec {
+		errors = append(errors, eris.Errorf("warning: %s identified as a glooctl plugin, but it is not executable", path))
+	} else if err != nil {
+		errors = append(errors, eris.Errorf("unable to identify %s as an executable file: %v", path, err))
+	}
+
+	if existingPath, ok := v.seenPlugins[binName]; ok {
+		errors = append(errors, eris.Errorf("warning: %s is overshadowed by a similarly named plugin: %s", path, existingPath))
+	} else {
+		v.seenPlugins[binName] = path
+	}
+
+	if cmd, _, err := v.root.Find(cmdPath); err == nil {
+		errors = append(errors, eris.Errorf("warning: %s overwrites existing command: %q", binName, cmd.CommandPath()))
+	}
+
+	return errors
+}
+
+func isExecutable(fullPath string) (bool, error) {
+	info, err := os.Stat(fullPath)
+	if err != nil {
+		return false, err
+	}
+
+	if runtime.GOOS == "windows" {
+		fileExt := strings.ToLower(filepath.Ext(fullPath))
+
+		switch fileExt {
+		case ".bat", ".cmd", ".com", ".exe", ".ps1":
+			return true, nil
+		}
+		return false, nil
+	}
+
+	if m := info.Mode(); !m.IsDir() && m&0111 != 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// uniquePathsList deduplicates a given slice of strings without
+// sorting or otherwise altering its order in any way.
+func uniquePathsList(paths []string) []string {
+	seen := map[string]bool{}
+	newPaths := []string{}
+	for _, p := range paths {
+		if seen[p] {
+			continue
+		}
+		seen[p] = true
+		newPaths = append(newPaths, p)
+	}
+	return newPaths
+}
+
+func hasValidPrefix(filepath string, validPrefixes []string) bool {
+	for _, prefix := range validPrefixes {
+		if !strings.HasPrefix(filepath, prefix+"-") {
+			continue
+		}
+		return true
+	}
+	return false
+}
--- projects/gloo/cli/pkg/cmd/plugin/list/root.go
+++ projects/gloo/cli/pkg/cmd/plugin/list/root.go
@@ -1,7 +1,10 @@
 package list
 
 import (
+	"fmt"
+
 	"github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/options"
+	"github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/plugin/common"
 	"github.com/solo-io/gloo/projects/gloo/cli/pkg/constants"
 	"github.com/spf13/cobra"
 )
@@ -11,13 +14,33 @@ func RootCmd(opts *options.Options) *cobra.Command {
 		Use:   constants.PLUGIN_LIST_COMMAND.Use,
 		Short: constants.PLUGIN_LIST_COMMAND.Short,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			o := PluginListOptions{
-				NameOnly: true,
-			}
-			if err := o.Complete(cmd); err != nil {
+			list, warnings, err := common.NewInstalledPluginRegistry(cmd).ListAll()
+			if err != nil {
 				return err
 			}
-			return o.Run()
+
+			if len(list) > 0 {
+				fmt.Println("The following plugins are installed:")
+				for _, plugin := range list {
+					fmt.Print(plugin.Name)
+					if len(plugin.Warnings) > 0 {
+						for _, err := range plugin.Warnings {
+							fmt.Printf("  - %s\n", err.Error())
+						}
+					} else {
+						fmt.Println("")
+					}
+				}
+			}
+
+			if len(warnings) > 0 {
+				fmt.Println("\n\nglooctl encountered the following warnings while searching for installed plugins:")
+				for _, warning := range warnings {
+					fmt.Println(warning.Error())
+				}
+			}
+
+			return nil
 		},
 	}
 	return cmd
